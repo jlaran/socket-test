7f73468e5bcb50d8f858d6c888df73e78fca7992
Juan.Lara <juan.lara@CR-HER-WKS16197.globalservs.com>
2017-06-23 08:48:36 -0600

    add new files

---
+++ client/main.js
@@ -0,0 +1,38 @@
+var socket = io.connect('https://socketstest.herokuapp.com',{'forceNew': true});
+
+socket.on('messages', function(data){
+	console.log(data);
+	render(data);
+});
+
+function render(data){
+	var html = data.map(function(message, index){
+		return (`
+				<div class="message">
+					<strong>${message.nickname} dice:</strong>
+					<p>${message.text}</p>
+				</div>
+			`);
+	}).join('');
+
+	var container = document.getElementById('mensajes');
+
+	container.innerHTML = html;
+	container.scrollTop = container.scrollHeight;
+}
+
+function addMessage(e){
+	var message = {
+		nickname: document.getElementById('nickname').value,
+		text: document.getElementById('text').value
+	};
+
+	document.getElementById('nickname').style.display = 'none';
+
+	socket.emit('add-message', message);
+
+	document.getElementById('nickname').value = "";
+	document.getElementById('text').value = "";
+
+	return false;
+}
