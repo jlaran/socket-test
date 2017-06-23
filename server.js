582cfe952883a6922e41cdc6448575b2a62e29b6
Juan C Lara <juanci@Juans-MacBook-Pro.local>
2017-06-23 10:20:00 -0600

    add files

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const server = express()
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
-  alert('Client connected');
+  console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
